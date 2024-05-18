62469574f69247edf0d75ca97a864ab8aa3877ae
조승주 <tmdwn3318@gmail.com>
2024-05-18 23:54:48 +0900

    메인 액티비티에 들어갈 프래그먼트 네개 만듦 메인 액티비티 java에 프래그먼트 옮기는 리스너 바텀 내비게이션에 추가

--- app/src/main/java/com/example/healthsnsproject/MainActivity.java
+++ app/src/main/java/com/example/healthsnsproject/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.healthsnsproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MainActivity extends AppCompatActivity {
+    //프래그먼트 객체 생성
+    Fragment_main_1 fragment_main_1;
+    Fragment_main_2 fragment_main_2;
+    Fragment_main_3 fragment_main_3;
+    Fragment_main_4 fragment_main_4;
+
 
 
     @Override
@@ -23,7 +34,39 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        //프래그먼트 할당
+        fragment_main_1 = new Fragment_main_1();
+        fragment_main_2 = new Fragment_main_2();
+        fragment_main_3 = new Fragment_main_3();
+        fragment_main_4 = new Fragment_main_4();
 
+        //1번 프래그먼트 컨테이너에 추가
+        getSupportFragmentManager().beginTransaction().add(R.id.container_main, fragment_main_1).commit();
+
+        //바텀 내비
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navagation);
+        bottomNavigationView.setOnItemSelectedListener(
+                new NavigationBarView.OnItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        int itemId = menuItem.getItemId();
+                        if (itemId == R.id.tab1) {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container_main, fragment_main_1).commit();
+                            return true;
+                        } else if (itemId == R.id.tab2) {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container_main, fragment_main_2).commit();
+                            return true;
+                        } else if (itemId == R.id.tab3) {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container_main, fragment_main_3).commit();
+                            return true;
+                        } else if (itemId == R.id.tab4) {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.container_main, fragment_main_4).commit();
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+        );
 
 
     }
