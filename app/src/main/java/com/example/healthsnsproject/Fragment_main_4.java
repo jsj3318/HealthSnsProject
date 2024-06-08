4262109593f4d04fed65e7c0f448749e9681a1d0
junhyeon <dhwnsgus99@naver.com>
2024-06-09 01:12:46 +0900

    - 프래그먼트 4 경고 보고 불필요한 선언같은거 고쳐서 경고 갯수 줄임 deprecated 경고나 건들면 기능 오류 생길거 같은 부분은 일부러 안건듬

--- app/src/main/java/com/example/healthsnsproject/Fragment_main_4.java
+++ app/src/main/java/com/example/healthsnsproject/Fragment_main_4.java
@@ -1,28 +1,20 @@
 package com.example.healthsnsproject;
 
-import static android.content.ContentValues.TAG;
-
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -32,48 +24,27 @@ import com.google.firebase.storage.UploadTask;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.Objects;
+
 public class Fragment_main_4 extends Fragment {
-    //프로필 사진 업로드 로딩 다이얼로그
-    private ProgressDialog progressDialog;
-
+    private ProgressDialog progressDialog; //프로필 사진 업로드 로딩 다이얼로그
     private Profile_view profileView;
-
     private  OnFragmentInteractionListener mListener;
 
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
     public Fragment_main_4() {
         // Required empty public constructor
-    }
-
-    public static Fragment_main_4 newInstance(String param1, String param2) {
-        Fragment_main_4 fragment = new Fragment_main_4();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-
 
         // 프로필 이미지 선택하는 인텐트 반환 받기
         if (requestCode == 1 && resultCode == getActivity().RESULT_OK && data != null) {
@@ -86,42 +57,33 @@ public class Fragment_main_4 extends Fragment {
                 StorageReference mountainsRef = storageRef.child("images/"+user.getDisplayName()+"/"+"profile/"+selectedImageUri.getLastPathSegment());//.child("user.jpg");
                 UploadTask uploadTask = mountainsRef.putFile(selectedImageUri);
 
-
-                Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
-                    @Override
-                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
-                        if (!task.isSuccessful()) {
-                            throw task.getException();
-                        }
-
-                        // Continue with the task to get the download URL
-                        return mountainsRef.getDownloadUrl();
+                uploadTask.continueWithTask(task -> {
+                    if (!task.isSuccessful()) {
+                        throw Objects.requireNonNull(task.getException());
                     }
-                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Uri> task) {
-                        //업로드 다이얼로그 종료
-                        progressDialog.dismiss();
 
-                        if (task.isSuccessful()) {
-                            Uri downloadUri = task.getResult();
-                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                    .setPhotoUri(downloadUri)
-                                    .build();
+                    // Continue with the task to get the download URL
+                    return mountainsRef.getDownloadUrl();
+                }).addOnCompleteListener(task -> {
+                    //업로드 다이얼로그 종료
+                    progressDialog.dismiss();
 
-                            user.updateProfile(profileUpdates);
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                .setPhotoUri(downloadUri)
+                                .build();
 
-                            Toast.makeText(getContext(), "이미지 업로드 성공", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(getContext(), "이미지 업로드 실패", Toast.LENGTH_SHORT).show();
-                        }
+                        user.updateProfile(profileUpdates);
+
+                        Toast.makeText(getContext(), "이미지 업로드 성공", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getContext(), "이미지 업로드 실패", Toast.LENGTH_SHORT).show();
                     }
                 });
 
-
             }
         }
-
 
     }
 
@@ -155,46 +117,28 @@ public class Fragment_main_4 extends Fragment {
         progressDialog.setMessage("프로필 이미지 업로드 중...");
 
         //프로필 이미지 클릭 리스너
-        profileView.setImageView_clickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setTitle("프로필 이미지 변경")
-                        .setMessage("프로필 이미지를 변경하시겠습니까?")
-                        .setPositiveButton("예", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // 갤러리에서 이미지 선택하는 인텐트 시작
-                                Intent intent = new Intent(Intent.ACTION_PICK);
-                                intent.setType("image/*");
-                                startActivityForResult(intent, 1);
+        profileView.setImageView_clickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+            builder.setTitle("프로필 이미지 변경")
+                    .setMessage("프로필 이미지를 변경하시겠습니까?")
+                    .setPositiveButton("예", (dialog, which) -> {
+                        // 갤러리에서 이미지 선택하는 인텐트 시작
+                        Intent intent = new Intent(Intent.ACTION_PICK);
+                        intent.setType("image/*");
+                        startActivityForResult(intent, 1);
 
-                                //업로드 로딩 다이얼로그 표시
-                                progressDialog.show();
-                            }
-                        })
-                        .setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // 사용자가 이미지 변경을 취소한 경우 아무 작업도 하지 않음
-                            }
-                        })
-                        .show();
-            }
+                        //업로드 로딩 다이얼로그 표시
+                        progressDialog.show();
+                    })
+                    .setNegativeButton("아니오", (dialog, which) -> {
+                        // 사용자가 이미지 변경을 취소한 경우 아무 작업도 하지 않음
+                    })
+                    .show();
         });
-
-
-
 
         //로그아웃 버튼 이벤트
         Button button = view.findViewById(R.id.button_logout);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mListener.Logout();
-            }
-        });
-
+        button.setOnClickListener(v -> mListener.Logout());
 
         return view;
     }
@@ -205,8 +149,7 @@ public class Fragment_main_4 extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new RuntimeException(context + " must implement OnFragmentInteractionListener");
         }
     }
 
